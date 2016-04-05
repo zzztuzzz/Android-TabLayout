4c6164f3d97f3e1c9939b4bfdd3dc3c3c0e89482
teiyuueki <tu.ue.et@gmail.com>
2016-04-06 02:57:38 +0900

    first commit

---
+++ app/src/main/java/com/example/teiyuueki/reginatablayout4/TabFlagment3.java
@@ -0,0 +1,19 @@
+package com.example.teiyuueki.reginatablayout4;
+
+/**
+ * Created by teiyuueki on 16/04/06.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabFlagment3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_3, container, false);
+    }
+}
